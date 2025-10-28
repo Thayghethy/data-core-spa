8c124d46ac3ee9e945b642e47a407b1c2f46ad3a
Thayghethy Souza dos Santos <101431239+Thayghethy@users.noreply.github.com>
2025-10-28 12:52:47 -0300

    Update tramitacaoData.ts

--- src/data/tramitacaoData.ts
+++ src/data/tramitacaoData.ts
@@ -1,12 +1,12 @@
 export const tramitacaoData = [
     // PRAPS
-    { Categoria: 'PRAPS', Falha: 'Porta Praps sem sincronismo', DSP: 'ESSE', FSP: 'ESSE', Destino: 'ESSE' },
-    { Categoria: 'PRAPS', Falha: 'Quedas de sincronismo', DSP: 'ESSE', FSP: 'ESSE', Destino: 'ESSE' },
-    { Categoria: 'PRAPS', Falha: 'Sem acesso ao modem Praps', DSP: 'ESSE', FSP: 'ESSE', Destino: 'ESSE' },
-    { Categoria: 'PRAPS', Falha: 'Erros ou atenuação', DSP: 'Diagnostico', FSP: 'Diagnostico', Destino: 'Diagnostico' },
-    { Categoria: 'PRAPS', Falha: 'Sem acesso ao DSLAM (com TA)', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Massiva' },
-    { Categoria: 'PRAPS', Falha: 'Sem acesso ao DSLAM e sem TA valida para associação', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
-    { Categoria: 'PRAPS', Falha: 'Porta LAN down no modem', DSP: 'ESSE', FSP: 'ESSE', Destino: 'ESSE' },
+    { Categoria: 'PRAPS', Falha: 'Porta Praps sem sincronismo', DSP: 'ESSE', FSP: '-', Destino: 'ESSE' },
+    { Categoria: 'PRAPS', Falha: 'Quedas de sincronismo', DSP: 'ESSE', FSP: '-', Destino: 'ESSE' },
+    { Categoria: 'PRAPS', Falha: 'Sem acesso ao modem Praps', DSP: 'ESSE', FSP: '-', Destino: 'ESSE' },
+    { Categoria: 'PRAPS', Falha: 'Erros ou atenuação', DSP: 'Diagnostico', FSP: '-', Destino: 'Diagnostico' },
+    { Categoria: 'PRAPS', Falha: 'Sem acesso ao DSLAM (com TA)', DSP: 'Pre_massiva', FSP: '-', Destino: 'Massiva' },
+    { Categoria: 'PRAPS', Falha: 'Sem acesso ao DSLAM e sem TA valida para associação', DSP: 'Pre_massiva', FSP: '-', Destino: 'Pre_massiva' },
+    { Categoria: 'PRAPS', Falha: 'Porta LAN down no modem', DSP: 'ESSE', FSP: 'ESSE', Destino: '-' },
 
     // CPE/ONU
     { Categoria: 'GPON', Falha: 'Porta/Sem sincronismo', DSP: 'ESSE', FSP: 'Regional V2', Destino: 'ESSE' },
@@ -21,28 +21,28 @@ export const tramitacaoData = [
     { Categoria: 'GPON', Falha: 'Sem acesso ao OLT e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
 
     // SWT
-    { Categoria: 'SWT', Falha: 'Interface entre SW/A e SWT em down', DSP: 'TX', FSP: 'Regional V2', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/DSL(S), SWT e TRACECELL', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/DSL(S) e SWT em down', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/A e SW IP down, porem SWT TRACE', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/E e SWT em down', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/A e SW (sem down/txdb)', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' },
-    { Categoria: 'SWT', Falha: 'Interface entre SW/A e SW (sem down/txdb)', DSP: 'TX', FSP: 'Regional/Parceiros', Destino: 'TX' }, // Duplicado na planilha, mantido para completude
-    { Categoria: 'SWT', Falha: 'Interface entre SWT/CPE em down', DSP: 'ESSE', FSP: 'Regional/Integrador', Destino: 'ESSE' },
-    { Categoria: 'SWT', Falha: 'Interface entre SWT/CPE cliente em down', DSP: 'ESSE', FSP: 'Regional/Parceiros', Destino: 'ESSE' },
-    { Categoria: 'SWT', Falha: 'Sem acesso ao SW/DSL(S) (com TA)', DSP: 'Massiva', FSP: 'Massiva', Destino: 'Massiva' },
-    { Categoria: 'SWT', Falha: 'Sem acesso ao SW/DSL(S) e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
-    { Categoria: 'SWT', Falha: 'Sem acesso ao SW/A (com TA)', DSP: 'Massiva', FSP: 'Massiva', Destino: 'Massiva' },
-    { Categoria: 'SWT', Falha: 'Sem acesso ao SW/A e sem TA válida', DSP: 'Massiva', FSP: 'Triagem Tens', Destino: 'Massiva' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWA x SWT em down', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWA x SWT em Up, porém SWT Inacessível', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWD/SWS x SWT em down', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWD/SWS x SWT em UP, porém SWT Inacessível', DSP: 'TX', FSP: 'RRegionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SHE x SSE em down', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWA x SHE em DOWN (radio)', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'TX' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWA x SHE em down (SDH - DWDM)', DSP: 'N2 TX', FSP: 'N2 TX', Destino: 'TX' }, // Duplicado na planilha, mantido para completude
+    { Categoria: 'SWT', Falha: 'Interface entre SWT x CPE Vivo em down', DSP: 'ESSE', FSP: 'Regional Integrador', Destino: 'ESSE' },
+    { Categoria: 'SWT', Falha: 'Interface entre SWTx CPE cliente em down', DSP: 'TX', FSP: 'Regionais Parceiros', Destino: 'ESSE' },
+    { Categoria: 'SWT', Falha: 'Sem acesso ao SWD/SWS (com TA)', DSP: 'Massiva', FSP: 'Massiva', Destino: 'Massiva' },
+    { Categoria: 'SWT', Falha: 'Sem acesso ao SWD/SWS e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
+    { Categoria: 'SWT', Falha: 'Sem acesso ao SWA (com TA)', DSP: 'Massiva', FSP: 'Massiva', Destino: 'Massiva' },
+    { Categoria: 'SWT', Falha: 'Sem acesso ao SWA e sem TA válida', DSP: 'Massiva', FSP: 'Triagem Tems', Destino: 'Massiva' },
 
     // Milegate
     { Categoria: 'Milegate', Falha: 'Porta Deslam em LOSS', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
-    { Categoria: 'Milegate', Falha: 'Sem acesso ao BDO', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
+    { Categoria: 'Milegate', Falha: 'Sem acesso ao EDD', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
     { Categoria: 'Milegate', Falha: 'Sem configuração', DSP: 'Diagnostico', FSP: 'Diagnostico', Destino: 'Diagnostico' },
     { Categoria: 'Milegate', Falha: 'Sem acesso ao elemento (com TA)', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Massiva' },
     { Categoria: 'Milegate', Falha: 'Sem acesso ao elemento e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
-    { Categoria: 'Milegate', Falha: 'Placa em plugada (com TA)', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Massiva' },
-    { Categoria: 'Milegate', Falha: 'Placa em plugada e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
-    { Categoria: 'Milegate', Falha: 'Porta Telcado em down', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
-    { Categoria: 'Milegate', Falha: 'Porta LAN down', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
+    { Categoria: 'Milegate', Falha: 'Placa em plugged (com TA)', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Massiva' },
+    { Categoria: 'Milegate', Falha: 'Placa em plugged e sem TA válida', DSP: 'Pre_massiva', FSP: 'Pre_massiva', Destino: 'Pre_massiva' },
+    { Categoria: 'Milegate', Falha: 'Porta Tellabs em down', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
+    { Categoria: 'Milegate', Falha: 'Lan EDD down', DSP: 'Regional V2', FSP: 'Regional V2', Destino: 'Regional V2' },
 ];
